70ca1bebfe8c427c7d629310e0844e9f0155c5b9
Camryn Sumabat <camrynsumabat@gmail.com>
2021-09-23 10:21:51 -0700

    Create basic crawler, add jsoup dependency

---
+++ src/main/java/WebCrawler.java
@@ -0,0 +1,51 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.HashSet;
+
+// code adapted from https://mkyong.com/java/jsoup-basic-web-crawler-example/
+public class WebCrawler {
+
+    private HashSet<String> links;
+    private int max = 5;
+
+    public WebCrawler() {
+        links = new HashSet<String>();
+    }
+
+    public void getPageLinks(String URL) {
+
+        // 4. Check if the URL has already been crawled and the page limit has not been reached
+        if (!links.contains(URL) && links.size() < max) {
+            try {
+
+                // 4a. If the URL has not been crawled yet, add it to the HashSet of links
+                if (links.add(URL)) {
+                    System.out.println(URL);
+                }
+
+                // 2. Fetch HTML code
+                Document document = Jsoup.connect(URL).get();
+
+                // 3. Parse the HTML and extract other URLs on the page
+                Elements linksOnPage = document.select("a[href]");
+                System.out.println("Number of outlinks: " + linksOnPage.size());
+
+                // 5. For each URL, go back to step 4
+                for (Element page : linksOnPage) {
+                    getPageLinks(page.attr("abs:href"));
+                }
+            } catch (IOException e) {
+                System.err.println("For '" + URL + "': " + e.getMessage());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        // 1. pick a seed URL
+        new WebCrawler().getPageLinks("http://www.cpp.edu");
+    }
+}
